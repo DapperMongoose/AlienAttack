f4fb928e30328be181b8763c2c637615a0e7a320
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-24 21:23:03 -0500

    completed the scene and movement lesson

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,5 +12,9 @@ func _physics_process(delta):
 		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
+	
+	var screen_size = get_viewport_rect().size	
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
 	move_and_slide()
 	

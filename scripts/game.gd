1db7a94cbaf34080d540902a7de64907bd126b11
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-25 22:14:26 -0500

    completed score UI lesson

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,6 +4,10 @@ var lives = 3
 var score = 0
 
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	hud.set_score_label(score)
 
 func _on_deathzone_area_entered(area):
 	area.die()
@@ -25,4 +29,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	
 func _on_enemy_died():
 	score += 100
-	print(score)
+	hud.set_score_label(score)

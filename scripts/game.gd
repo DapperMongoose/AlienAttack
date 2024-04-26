dfb40f19da15de6282604e122d4daa24a445ccd8
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-26 17:55:47 -0500

    completed sound effects lesson

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,8 +4,12 @@ var lives = 3
 var score = 0
 
 @onready var player = $Player
+
 @onready var hud = $UI/HUD
 @onready var ui = $UI
+
+@onready var enemy_hit_sound = $EnemyHitSound
+@onready var player_take_damage_sound = $PlayerTakeDamageSound
 
 var game_over_screen = preload("res://scenes/game_over_screen.tscn")
 
@@ -19,14 +23,14 @@ func _on_deathzone_area_entered(area):
 
 func _on_player_took_damage():
 	lives -= 1
+	player_take_damage_sound.play()
+	hud.set_lives(lives)
 	if lives == 0:
 		player.die()
 		await get_tree().create_timer(1.5).timeout
 		var game_over_instance = game_over_screen.instantiate()
 		game_over_instance.set_score(score)
 		ui.add_child(game_over_instance)
-	else:
-		hud.set_lives(lives)
 
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
@@ -37,3 +41,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 func _on_enemy_died():
 	score += 100
 	hud.set_score_label(score)
+	enemy_hit_sound.play()

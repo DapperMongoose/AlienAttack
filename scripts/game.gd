4842cf8c8e51b337045166765cfd17cde18a208b
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-25 22:37:39 -0500

    completed game over screen lesson

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,20 +5,26 @@ var score = 0
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var game_over_screen = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
 	hud.set_score_label(score)
 	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
-	area.die()
+	area.queue_free()
 
 
 func _on_player_took_damage():
 	lives -= 1
 	if lives == 0:
-		print("Game Over")
 		player.die()
+		await get_tree().create_timer(1.5).timeout
+		var game_over_instance = game_over_screen.instantiate()
+		game_over_instance.set_score(score)
+		ui.add_child(game_over_instance)
 	else:
 		hud.set_lives(lives)
 
